9e32f15339b022df436569b9cc9efaf80a4f7eab
John Lopez <jlopez@timewarptechnologies.com>
2025-07-15 16:45:58 -0700

    Initial version

---
+++ wmi-monitor/WMIMonitor.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Management;
+using System.Diagnostics;
+
+class WMIMonitor
+{
+    public static void MonitorWMIActivity()
+    {
+        try
+        {
+            var query = new WqlEventQuery(
+                "SELECT * FROM __InstanceOperationEvent " +
+                "WITHIN 1 " +
+                "WHERE TargetInstance ISA '__Win32Provider'"
+            );
+
+            var watcher = new ManagementEventWatcher(query);
+            watcher.EventArrived += (sender, e) =>
+            {
+                var targetInstance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+                Console.WriteLine($"Provider: {targetInstance["Name"]}");
+                Console.WriteLine($"Namespace: {targetInstance["Namespace"]}");
+                Console.WriteLine($"Host Process: {targetInstance["HostProcessIdentifier"]}");
+            };
+
+            watcher.Start();
+            Console.WriteLine("Monitoring WMI activity... Press Enter to stop.");
+            Console.ReadLine();
+            watcher.Stop();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
